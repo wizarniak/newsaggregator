67c7aa64daab611b978bc5a801ec029231b9fd24
Qurat <dragon.ace2020@gmail.com>
2020-06-29 21:10:00 +0400

    Aggregate news from reddit and newsapi

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import json
 
 app = FastAPI()
 
+MAX_RESULTS = 5
+
 @app.get("/")
 async def home():
     return {"message": "Go to /news to get news"}
@@ -13,12 +15,13 @@ async def home():
 @app.get("/news")
 async def get_news(query: str = None):
     reddit_access_token = get_reddit_access_token()
-    print(reddit_access_token)
     if query:
-        return {"message": "News search"}
+        reddit_news = search_reddit_news(reddit_access_token, query)
+        newsapi_news = search_newsapi_news(query)
     else:
         reddit_news = get_reddit_news(reddit_access_token)
-        return reddit_news
+        newsapi_news = get_newsapi_news()
+    return reddit_news + newsapi_news
 
 def get_reddit_access_token():
     url = "https://www.reddit.com/api/v1/access_token"
@@ -28,7 +31,57 @@ def get_reddit_access_token():
     return json.loads(response.content)["access_token"]
 
 def get_reddit_news(access_token):
-    url = "https://oauth.reddit.com/r/worldnews/hot?limit=5"
+    url = "https://oauth.reddit.com/r/worldnews/hot"
+    params = {"limit": MAX_RESULTS}
     response = requests.get(url,
-    headers={"Authorization": "bearer " + access_token, "User-Agent":config.reddit_user_agent})
-    return response.content
+    headers={"Authorization": "bearer " + access_token, "User-Agent":config.reddit_user_agent},
+    params = params)
+    news = []
+    for item in json.loads(response.content)["data"]["children"]:
+        news_item = {"headline": item["data"]["title"], 
+        "link": item["data"]["url"],
+        "source": "reddit"}
+        news.append(news_item)
+    return news
+
+def search_reddit_news(access_token, query):
+    url = "https://oauth.reddit.com/r/worldnews/search"
+    params = {"limit": MAX_RESULTS, "q": query}
+    response = requests.get(url,
+    headers={"Authorization": "bearer " + access_token, "User-Agent":config.reddit_user_agent, "q": query},
+    params = params)
+    news = []
+    for item in json.loads(response.content)["data"]["children"]:
+        news_item = {"headline": item["data"]["title"], 
+        "link": item["data"]["url"],
+        "source": "reddit"}
+        news.append(news_item)
+    return news
+
+def get_newsapi_news():
+    url = "https://newsapi.org/v2/top-headlines"
+    params = {"category": "general", "pageSize": 5}
+    response = requests.get(url,
+    headers={"Authorization": "bearer " + config.news_api_key},
+    params = params)
+    news = []
+    for item in json.loads(response.content)["articles"]:
+        news_item = {"headline": item["title"],
+        "link": item["url"],
+        "source": "newsapi"}
+        news.append(news_item)
+    return news
+
+def search_newsapi_news(query):
+    url = "https://newsapi.org/v2/everything"
+    params = {"pageSize": 5, "q": query, "language": "en", "sortBy": "relevancy"}
+    response = requests.get(url,
+    headers={"Authorization": "bearer " + config.news_api_key},
+    params = params)
+    news = []
+    for item in json.loads(response.content)["articles"]:
+        news_item = {"headline": item["title"],
+        "link": item["url"],
+        "source": "newsapi"}
+        news.append(news_item)
+    return news
